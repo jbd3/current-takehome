19af48577c87336bb70608b7f8656f70e2d4ef08
Jake Douglas <jbrandondouglas@gmail.com>
2020-04-30 17:11:51 -0400

    simple api

---
+++ api/visit.js
@@ -0,0 +1,112 @@
+const { MongoClient, ObjectId } = require('mongodb');
+const FuzzySearch = require('fuzzy-search');
+
+async function connectToDatabase(uri) {
+  try {
+    const client = await MongoClient.connect(uri, { useNewUrlParser: true })
+    const db = await client.db('current')
+    return db
+  } catch (err) {
+    throw new Error(`Error connecting to DB: ${err}`)
+  }
+}
+
+module.exports = async (req, res) => {
+  const db = await connectToDatabase(process.env.MONGODB_URI)
+
+  // visitsCollection { _id<uId> (used as visitId), userId<string>, name<string> }
+  const visitsCollection = await db.collection('visits')
+  // usersCollection { _id<uId>, userId<string>, arrivalObjects<[{ visitId, userId, name }]> }
+  const usersCollection = await db.collection('users')
+
+  const { method } = req;
+  if (method === 'POST') {
+    const { userId, name } = req.body;
+    if (!userId || !name) {
+      return res.status(404).send({
+        message: 'Invalid request, please specify userId and name in request body'
+      });
+    }
+    const arrivalObject = { userId, name };
+    let visitId;
+    try {
+      const newDocument = await visitsCollection.insertOne(arrivalObject);
+      visitId = newDocument.insertedId;
+      arrivalObject.visitId = visitId;
+    } catch (err) {
+      console.error('Error adding arrival object to visitsCollection: ', err);
+      return res.status(500).send();
+    }
+    try {
+      const userDocument = await usersCollection.findOne({ userId });
+      if (!userDocument) {
+        await usersCollection.insertOne({
+          userId,
+          arrivalObjects: [arrivalObject],
+        })
+      } else {
+        const { arrivalObjects } = userDocument;
+        arrivalObjects.push(arrivalObject);
+        if (arrivalObjects.length > 5) {
+          arrivalObjects.shift()
+        }
+        await usersCollection.findOneAndUpdate({ userId }, { $set: { arrivalObjects } }, { returnOriginal: false });
+      }
+      return res.status(200).json({ visitId })
+    } catch (err) {
+      console.error('Error adding arrival object to usersCollection: ', err);
+      return res.status(500).send();
+    }
+  }
+
+  if (method === 'GET') {
+    const { visitId, userId, searchString } = req.query;
+    if (visitId) {
+      try {
+        const visit = await visitsCollection.findOne({ "_id": ObjectId(visitId) })
+        if (!visit) {
+          return res.status(404).send({
+            message: `No visit found for visitId: ${visitId}}`
+          });
+        }
+        const arrivalObject = {
+          userId: visit.userId,
+          name: visit.name,
+          visitId,
+        }
+        res.status(200).json([arrivalObject])
+      } catch (err) {
+        console.error('Error finding visit from visitId: ', err);
+        return res.status(500).send();
+      }
+    } else if (userId && searchString) {
+      try {
+        const userVisits = await usersCollection.findOne({ userId })
+        if (!userVisits) {
+          return res.status(404).send({
+            message: `No user found for userId: ${userId}`
+          });
+        }
+        const { arrivalObjects } = userVisits;
+        const searcher = new FuzzySearch(arrivalObjects, ['name'], {
+          caseSensitive: false,
+          sort: true, // sorted by best match
+        });
+        const results = searcher.search(searchString)
+        const matchedArrivalObjects = results.map(({ userId, name, visitId }) => ({ userId, name, visitId }))
+        return res.status(200).json(matchedArrivalObjects)
+      } catch (err) {
+        console.error('Error finding visits from userId and searchString: ', err);
+        return res.status(500).send();
+      }
+    } else {
+      return res.status(404).send({
+        message: `Invalid request, please specify unique visitorId or userId and searchString`
+      });
+    }
+  }
+
+  return res.status(404).send({
+    message: 'Invalid request, please either POST a valid visit or GET visit info with valid query params'
+  });
+}
